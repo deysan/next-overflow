91395790af95b88d0524876e82c130c937a00b6d
Andrii Deisan <ideysan@gmail.com>
2023-10-06 22:57:21 +0300

    Add assets and update layout

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from '@clerk/nextjs';
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1 className="h1-bold">Home Page</h1>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
