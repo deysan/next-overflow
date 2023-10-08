d18f76d309810ee2497ec0dab23e937ba3df64a7
Andrii Deisan <ideysan@gmail.com>
2023-10-08 21:02:51 +0300

    Update home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,25 @@
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="h1-bold">Home Page</h1>
-    </div>
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+            Ask a Question
+          </Link>
+        </Button>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        LocalSearchbar Filters
+      </div>
+    </>
   );
 }
