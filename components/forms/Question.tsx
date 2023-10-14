c757092fdf4b888b716218a33cd3d4d26a9ae658
Andrii Deisan <ideysan@gmail.com>
2023-10-14 17:52:53 +0300

    Add tiny editor

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import { Editor as TinyMCEEditor } from 'tinymce';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { useForm } from 'react-hook-form';
@@ -17,6 +20,8 @@ import { Button } from '../ui/button';
 import { QuestionsSchema } from '@/lib/validations';
 
 export default function Question() {
+  const editorRef = useRef<TinyMCEEditor | null>(null);
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
@@ -77,7 +82,37 @@ export default function Question() {
               </FormLabel>
 
               <FormControl className="mt-3.5">
-                {/* TODO: Add an editor component */}
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(evt, editor) => (editorRef.current = editor)}
+                  initialValue=""
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      'advlist',
+                      'autolink',
+                      'lists',
+                      'link',
+                      'image',
+                      'charmap',
+                      'preview',
+                      'anchor',
+                      'searchreplace',
+                      'visualblocks',
+                      'codesample',
+                      'fullscreen',
+                      'insertdatetime',
+                      'media',
+                      'table',
+                    ],
+                    toolbar:
+                      'undo redo | ' +
+                      'codesample | bold italic forecolor | alignleft aligncenter |' +
+                      'alignright alignjustify | bullist numlist',
+                    content_style: 'body { font-family:Inter; font-size:16px }',
+                  }}
+                />
               </FormControl>
 
               <FormDescription className="body-regular mt-2.5 text-light-500">
