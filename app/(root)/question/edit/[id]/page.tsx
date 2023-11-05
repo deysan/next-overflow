0f68d5f503894132eca1e186ac783feda458cc18
Andrii Deisan <ideysan@gmail.com>
2023-11-05 23:49:17 +0200

    Add update question component

---
+++ app/(root)/question/edit/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Edit Question</h1>
+    </>
+  );
+}
--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,8 +1,10 @@
+import { formatAndDivideNumber, getTimestamp } from '@/lib/utils';
+import { SignedIn } from '@clerk/nextjs';
 import Link from 'next/link';
 import React from 'react';
-import RenderTag from '../shared/RenderTag';
-import { formatAndDivideNumber, getTimestamp } from '@/lib/utils';
+import EditDeleteAction from '../shared/EditDeleteAction';
 import Metric from '../shared/Metric';
+import RenderTag from '../shared/RenderTag';
 
 export default function QuestionCard({
   _id,
@@ -32,6 +34,12 @@ export default function QuestionCard({
   createdAt: Date;
   clerkId?: string | null;
 }) {
+  const showActionButtons = clerkId && clerkId === author.clerkId;
+  console.log(
+    '🚀 ~ file: QuestionCard.tsx:37 ~ showActionButtons:',
+    showActionButtons
+  );
+
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
@@ -47,7 +55,11 @@ export default function QuestionCard({
           </Link>
         </div>
 
-        {/* If signed in add edit delete actions */}
+        <SignedIn>
+          {showActionButtons && (
+            <EditDeleteAction type="Question" itemId={JSON.stringify(_id)} />
+          )}
+        </SignedIn>
       </div>
 
       <div className="mt-3.5 flex flex-wrap gap-2">
